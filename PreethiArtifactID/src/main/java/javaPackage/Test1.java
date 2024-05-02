dbb56ceb0bffc573a99b10a67eb79f81504cfb08
Amith Gautam <Amith Gautam@LAPTOP-UC8VD6T3>
2024-05-02 14:58:32 +0530

    Add TestProject

---
+++ PreethiArtifactID/src/main/java/javaPackage/Test1.java
@@ -0,0 +1,18 @@
+package javaPackage;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void a() {
+		System.out.println("Hello TestNG");
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello main method");
+
+	}
+
+}
